b44490d26ad74fe4ee1135182bea671074b7c177
zigl1 <ilgiz7725@gmail.com>
2021-02-03 14:37:08 -0500

    added TextBoxTest(finish)

--- src/test/java/test/TextBoxTest.java
+++ src/test/java/test/TextBoxTest.java
@@ -17,45 +17,45 @@ public class TextBoxTest {
         Configuration.startMaximized = true;
 
     }
-   
+
 
     @Test
     void dataAppearsInOutputBlockTest() {
-    String name ="Ilgiz";
-    open("https://demoqa.com/automation-practice-form");
-    $(".main-header").shouldHave(text("Practice Form"));
-    $("#firstName").setValue(name);
-    $("#lastName").setValue("Student");
-    $("#userEmail").setValue("student@gmail.com");
-    $("label[for='gender-radio-1']").click();
-    $("#userNumber").setValue("1234567890");
-    //DateOfBirth
-    $("#dateOfBirthInput").click();
-    $(".react-datepicker__year-select").click();
-    $("option[value='1995']").click();
-    $(".react-datepicker__month-select").click();
-    $("option[value='1']").click();
-    $(".react-datepicker__day.react-datepicker__day--025").click();
-    $("#subjectsInput").setValue("Computer Science").pressEnter();
-    $("label[for='hobbies-checkbox-1']").click();
-    $("#uploadPicture").setValue("C:\\Users\\ilgiz\\Desktop\\screenshot.test.png");
-    $("#currentAddress").setValue("12356 Haha");
-    $("#state").click();
-    $(By.xpath("//div[contains(text(),'Rajasthan')]")).click();
-    $("#city").click();
-    $(By.xpath("//div[contains(text(),'Jaipur')]")).click();
-    $("#submit").click();
-    $(".modal-content").shouldHave(text("Thanks for submitting the form"),
-                                   text("Ilgiz Student"),
-                                   text("student@gmail.com"),
-                                   text("Male"),
-                                   text("1234567890"),
-                                   text("25 February,1995"),
-                                   text("Computer Science"),
-                                   text("Sports"),
-                                   text("screenshot.test.png"),
-                                   text("12356 Haha"),
-                                   text("Rajasthan Jaipur"));
+        String name = "Ilgiz";
+        open("https://demoqa.com/automation-practice-form");
+        $(".main-header").shouldHave(text("Practice Form"));
+        $("#firstName").setValue(name);
+        $("#lastName").setValue("Student");
+        $("#userEmail").setValue("student@gmail.com");
+        $("label[for='gender-radio-1']").click();
+        $("#userNumber").setValue("1234567890");
+        //DateOfBirth
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").click();
+        $("option[value='1995']").click();
+        $(".react-datepicker__month-select").click();
+        $("option[value='1']").click();
+        $(".react-datepicker__day.react-datepicker__day--025").click();
+        $("#subjectsInput").setValue("Computer Science").pressEnter();
+        $("label[for='hobbies-checkbox-1']").click();
+        $("#uploadPicture").setValue("C:\\Users\\ilgiz\\Desktop\\screenshot.test.png");
+        $("#currentAddress").setValue("12356 Haha");
+        $("#state").click();
+        $(By.xpath("//div[contains(text(),'Rajasthan')]")).click();
+        $("#city").click();
+        $(By.xpath("//div[contains(text(),'Jaipur')]")).click();
+        $("#submit").click();
+        $(".modal-content").shouldHave(text("Thanks for submitting the form"),
+                text("Ilgiz Student"),
+                text("student@gmail.com"),
+                text("Male"),
+                text("1234567890"),
+                text("25 February,1995"),
+                text("Computer Science"),
+                text("Sports"),
+                text("screenshot.test.png"),
+                text("12356 Haha"),
+                text("Rajasthan Jaipur"));
 
     }
 
