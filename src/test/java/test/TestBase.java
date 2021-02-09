27a4bfc2a2b43619d25683c92a06aad5c5abcf26
zigl1 <ilgiz7725@gmail.com>
2021-02-08 22:45:12 -0500

    added Faker(finish)

---
+++ src/test/java/test/TestBase.java
@@ -0,0 +1,11 @@
+package test;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+    @BeforeAll
+    static void setup() {
+        Configuration.startMaximized = true;
+    }
+}
--- src/test/java/test/TextBoxTest.java
+++ src/test/java/test/TextBoxTest.java
@@ -1,61 +1,75 @@
 package test;
 
 import com.codeborne.selenide.Configuration;
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
+
+import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class TextBoxTest {
-
+public class TextBoxTest extends TestBase {
 
     @BeforeAll
     static void setup() {
         Configuration.startMaximized = true;
-
     }
-
 
     @Test
     void dataAppearsInOutputBlockTest() {
-        String name = "Ilgiz";
+        //Test data
+        Faker faker = new Faker();
+        String firstname = faker.name().firstName(),
+                lastname = faker.name().lastName(),
+                email = faker.internet().emailAddress("test"),
+                gender = "Male",
+                mobile = faker.phoneNumber().subscriberNumber(10),
+                month = "February",
+                year = "1995",
+                day = "25",
+                subject = "Computer Science",
+                fileloc = "src/test/resources/Untitled2.png",
+                filename = "Untitled2.png",
+                address = faker.address().fullAddress(),
+                state = "Rajasthan",
+                city = "Jaipur";
+
         open("https://demoqa.com/automation-practice-form");
         $(".main-header").shouldHave(text("Practice Form"));
-        $("#firstName").setValue(name);
-        $("#lastName").setValue("Student");
-        $("#userEmail").setValue("student@gmail.com");
-        $("label[for='gender-radio-1']").click();
-        $("#userNumber").setValue("1234567890");
+        $("#firstName").setValue(firstname);
+        $("#lastName").setValue(lastname);
+        $("#userEmail").setValue(email);
+        $("#genterWrapper").$(byText(gender)).click();
+        $("#userNumber").setValue(mobile);
         //DateOfBirth
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__year-select").click();
-        $("option[value='1995']").click();
-        $(".react-datepicker__month-select").click();
-        $("option[value='1']").click();
+        $(".react-datepicker__year-select").selectOption(year);
+        $(".react-datepicker__month-select").selectOption(month);
         $(".react-datepicker__day.react-datepicker__day--025").click();
-        $("#subjectsInput").setValue("Computer Science").pressEnter();
+        $("#subjectsInput").setValue(subject).pressEnter();
         $("label[for='hobbies-checkbox-1']").click();
-        $("#uploadPicture").setValue("C:\\Users\\ilgiz\\Desktop\\screenshot.test.png");
-        $("#currentAddress").setValue("12356 Haha");
+        $("#uploadPicture").uploadFile(new File(fileloc));
+        $("#currentAddress").setValue(address);
         $("#state").click();
-        $(By.xpath("//div[contains(text(),'Rajasthan')]")).click();
+        $("#stateCity-wrapper").$(byText(state)).click();
         $("#city").click();
-        $(By.xpath("//div[contains(text(),'Jaipur')]")).click();
+        $("#stateCity-wrapper").$(byText(city)).click();
         $("#submit").click();
         $(".modal-content").shouldHave(text("Thanks for submitting the form"),
-                text("Ilgiz Student"),
-                text("student@gmail.com"),
-                text("Male"),
-                text("1234567890"),
-                text("25 February,1995"),
-                text("Computer Science"),
+                text(firstname + " " + lastname),
+                text(email),
+                text(gender),
+                text(mobile),
+                text(day + " " + month + "," + year),
+                text(subject),
                 text("Sports"),
-                text("screenshot.test.png"),
-                text("12356 Haha"),
-                text("Rajasthan Jaipur"));
+                text(filename),
+                text(address),
+                text(state + " " + city));
     }
 
 }
